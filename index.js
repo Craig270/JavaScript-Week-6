09144170ac16b27010cd45dde9a18bb53d9a495c
Craig Neff <ifly12610@gmail.com>
2021-03-05 22:02:22 -0700

    really proud of this game. I build everything but the shuffle without google.

--- index.js
+++ index.js
@@ -93,17 +93,13 @@ class Menu extends Player {
           break;
         case "2":
           this.startWar();
-          break;
-          break;
-        case "3":
-          this.alertPlayers();
-          break;
+          return;
         default:
           selection = 0;
       }
       selection = this.showMainMenu();
     }
-    alert(`Thank you for visiting this virtual War-Card Game simulator
+    alert(`Thank you for visiting this virtual War-Card Game simulator!
     
     If you would like to play again simply refresh this page.`);
   }
@@ -115,7 +111,6 @@ class Menu extends Player {
     0) Exit
     1) Add Players for a new Game
     2) Deal Cards and Start the War Simulator!
-    3) Alert Player1 and Player2 Names.
     `);
   }
 
@@ -134,64 +129,72 @@ class Menu extends Player {
     let player2 = prompt(`Enter the name for Player2: `);
     this.player1Picked = new Player(player1);
     this.player2Picked = new Player(player2);
-    return this.showMainMenu();
   }
   startWar() {
-    alert(
-      `Make sure you open your console in browser to see the Game! Also you may need to type 0 on the main menu to close the alert window.`
-    );
-    //Makes a new deck of cards and shuffles them
-    let startWarDeck1 = new Deck();
-    startWarDeck1.makeAdeck();
-    startWarDeck1.shuffle(startWarDeck1.newDeck);
-    // For loop for dealing out the card to each player's hand
-    for (let i = 0; i < startWarDeck1.newDeck.length; i++) {
-      if (i % 2 == 0) {
-        this.player1Picked.hand.push(startWarDeck1.newDeck[i]);
-      } else {
-        this.player2Picked.hand.push(startWarDeck1.newDeck[i]);
+    if (
+      this.player1Picked instanceof Player &&
+      this.player2Picked instanceof Player
+    ) {
+      alert(`Make sure you open your browser's console to see the Game!
+    
+    Refresh this page if you want to simulate another game.`);
+      //Makes a new deck of cards and shuffles them
+      let startWarDeck1 = new Deck();
+      startWarDeck1.makeAdeck();
+      startWarDeck1.shuffle(startWarDeck1.newDeck);
+      // For loop for dealing out the card to each player's hand
+      for (let i = 0; i < startWarDeck1.newDeck.length; i++) {
+        if (i % 2 == 0) {
+          this.player1Picked.hand.push(startWarDeck1.newDeck[i]);
+        } else {
+          this.player2Picked.hand.push(startWarDeck1.newDeck[i]);
+        }
       }
-    }
-    //For loop for the comparison between card values. 2 lowest 14 highest
-    for (let i = 0; i < this.player1Picked.hand.length; i++) {
-      if (this.player1Picked.hand[i].value > this.player2Picked.hand[i].value) {
-        this.player1Score += 1;
-        console.log(
-          `${this.player1Picked.name} played ${this.player1Picked.hand[i].rank} of ${this.player1Picked.hand[i].suit} beating out ${this.player2Picked.hand[i].rank} of ${this.player2Picked.hand[i].suit} played by ${this.player2Picked.name}!`
-        );
-      } else if (
-        this.player1Picked.hand[i].value < this.player2Picked.hand[i].value
-      ) {
-        this.player2Score += 1;
-        console.log(
-          `${this.player2Picked.name} played ${this.player2Picked.hand[i].rank} of ${this.player2Picked.hand[i].suit} beating out ${this.player1Picked.hand[i].rank} of ${this.player1Picked.hand[i].suit} played by ${this.player1Picked.name}`
-        );
-      } else if (
-        this.player1Picked.hand[i].value == this.player2Picked.hand[i].value
-      ) {
-        console.log(`This battle of war was a tie! No player got a point!`);
+      //For loop for the comparison between card values. 2 lowest 14 highest
+      for (let i = 0; i < this.player1Picked.hand.length; i++) {
+        if (
+          this.player1Picked.hand[i].value > this.player2Picked.hand[i].value
+        ) {
+          this.player1Score += 1;
+          console.log(
+            `${this.player1Picked.name} played ${this.player1Picked.hand[i].rank} of ${this.player1Picked.hand[i].suit} beating out ${this.player2Picked.hand[i].rank} of ${this.player2Picked.hand[i].suit} played by ${this.player2Picked.name}!`
+          );
+        } else if (
+          this.player1Picked.hand[i].value < this.player2Picked.hand[i].value
+        ) {
+          this.player2Score += 1;
+          console.log(
+            `${this.player2Picked.name} played ${this.player2Picked.hand[i].rank} of ${this.player2Picked.hand[i].suit} beating out ${this.player1Picked.hand[i].rank} of ${this.player1Picked.hand[i].suit} played by ${this.player1Picked.name}!`
+          );
+        } else if (
+          this.player1Picked.hand[i].value == this.player2Picked.hand[i].value
+        ) {
+          console.log(`This battle of war was a tie! No player got a point!`);
+        }
       }
-    }
-    console.log(`
+      console.log(`
     --------------
     The Score:
     --------------
     ${this.player1Picked.name}:${this.player1Score}
     ${this.player2Picked.name}:${this.player2Score}
     `);
-    if (this.player1Score > this.player2Score) {
-      console.log(
-        `Congratulations!! ${this.player1Picked.name} you are the winner of this War Card Game simuilated game!!`
-      );
-    }
-    if (this.player2Score > this.player1Score) {
-      console.log(
-        `Congratulations!! ${this.player2Picked.name} you are the winner of this War Card Game simuilated game!!`
-      );
-    } else if (this.player1Score === this.player2Score) {
-      console.log(
-        `As luck would have it this game was a tie and no one is the winner!`
-      );
+      if (this.player1Score > this.player2Score) {
+        console.log(
+          `Congratulations!! ${this.player1Picked.name}, you are the winner of this War Card Game simuilated game!!`
+        );
+      }
+      if (this.player2Score > this.player1Score) {
+        console.log(
+          `Congratulations!! ${this.player2Picked.name} you are the winner of this War Card Game simuilated game!!`
+        );
+      } else if (this.player1Score === this.player2Score) {
+        console.log(
+          `As luck would have it this game was a tie and no one is the winner!`
+        );
+      }
+    } else {
+      throw new Error(`You must first add players with option 1`);
     }
   }
 }
